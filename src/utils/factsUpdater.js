784f03f8c1ad3847f5af1ca813c191a4fba3759c
Patrick Ferreira <paatrickferreira@gmail.com>
2018-03-26 16:31:05 -0300

    Add functions to calculate goals scores and test suites for them

--- src/utils/factsUpdater.js
+++ src/utils/factsUpdater.js
@@ -12,12 +12,26 @@ const isDraw = (userScore, opponentScore) => userScore == opponentScore;
 
 const updateScore = (score) => score + 1;
 
+const updateGoalsScore = (currentScoredGoals, matchScoredGoals) => currentScoredGoals + matchScoredGoals;
+
+const updateGoalsConceded = (currentConcededGoals, matchConcededGoals) => currentConcededGoals + matchConcededGoals;
+
+const updateGoalsDifference = (matchScoredGoals, matchConcededGoals, {goalsScored, goalsConceded}) => {
+  const currentScoredGoals = matchScoredGoals + goalsScored;
+  const currentConcededGoals = matchConcededGoals + goalsConceded;
+  
+  return currentScoredGoals - currentConcededGoals;
+};
+
 const updateUserFacts = (user, userScore, opponentScore) => {
   return {
     playedMatches: updateScore(user.playedMatches),
     won: isWin(userScore, opponentScore) ? updateScore(user.won) : user.won,
     loss: isLoss(userScore, opponentScore) ? updateScore(user.loss) : user.loss,
     draw: isDraw(userScore, opponentScore) ? updateScore(user.draw) : user.draw,
+    goalsScored: updateGoalsScore(user.goalsScored, userScore),
+    goalsConceded: updateGoalsConceded(user.goalsConceded, opponentScore),
+    goalsDifference: updateGoalsDifference(userScore, opponentScore, user)
   };
 };
 
@@ -26,5 +40,8 @@ module.exports = {
   isDraw,
   isLoss,
   updateScore,
-  updateUserFacts
+  updateUserFacts,
+  updateGoalsScore,
+  updateGoalsConceded,
+  updateGoalsDifference
 };
--- test/factsUpdater.test.js
+++ test/factsUpdater.test.js
@@ -24,50 +24,56 @@ describe('Should update user leaderboard facts correctly', () => {
     won: 0,
     loss: 0,
     draw: 0,
+    goalsScored: 0,
+    goalsConceded: 0,
+    goalsDifference: 0
   };
 
   test('works when user has won', () => {
-    const match = {homeScore: 3, awayScore: 2};
-  
-    const expectedUserFacts = {
-      playedMatches: 1,
-      won: 1,
-      loss: 0,
-      draw: 0,
-    };
+    const match = { homeScore: 3, awayScore: 2 };
+    const expectedUserFacts = { playedMatches: 1, won: 1 };
   
     const resp = facts.updateUserFacts(defaultUserFacts, match.homeScore, match.awayScore);
-  
     expect(resp).toMatchObject(expectedUserFacts);
   });
 
   test('works when match has been a draw', () => {
-    const match = {homeScore: 3, awayScore: 3};
-  
-    const expectedUserFacts = {
-      playedMatches: 1,
-      won: 0,
-      loss: 0,
-      draw: 1,
-    };
+    const match = { homeScore: 3, awayScore: 3 };
+    const expectedUserFacts = { playedMatches: 1, draw: 1 };
   
     const resp = facts.updateUserFacts(defaultUserFacts, match.homeScore, match.awayScore);
-  
     expect(resp).toMatchObject(expectedUserFacts);
   });
 
   test('works when user has loss', () => {
-    const match = {homeScore: 2, awayScore: 3};
-  
-    const expectedUserFacts = {
-      playedMatches: 1,
-      won: 0,
-      loss: 1,
-      draw: 0,
-    };
+    const match = { homeScore: 2, awayScore: 3 };
+    const expectedUserFacts = { playedMatches: 1, loss: 1 };
   
     const resp = facts.updateUserFacts(defaultUserFacts, match.homeScore, match.awayScore);
-  
     expect(resp).toMatchObject(expectedUserFacts);
   });
-})
+
+  test('works when user scores goals', () => {
+    const match = { homeScore: 3, awayScore: 2 };
+    const expectedUserFacts = { goalsScored: 3 };
+
+    const resp = facts.updateUserFacts(defaultUserFacts, match.homeScore, match.awayScore);
+    expect(resp).toMatchObject(expectedUserFacts);
+  });
+
+  test('works when user concedes goals', () => {
+    const match = { homeScore: 2, awayScore: 3 };
+    const expectedUserFacts = { goalsConceded: 3 };
+
+    const resp = facts.updateUserFacts(defaultUserFacts, match.homeScore, match.awayScore);
+    expect(resp).toMatchObject(expectedUserFacts);
+  });
+
+  test('works when calculating goals difference', () => {
+    const match = { homeScore: 3, awayScore: 2 };
+    const expectedUserFacts = { goalsDifference: 1 };
+
+    const resp = facts.updateUserFacts(defaultUserFacts, match.homeScore, match.awayScore);
+    expect(resp).toMatchObject(expectedUserFacts);
+  });
+});
