d1f56f323434df70210626522d9fb377e8b3a1eb
Patrick Ferreira <paatrickferreira@gmail.com>
2018-03-27 18:30:25 -0300

    Remove unnecessary position field from leaderboard schema

--- src/models/LeaderBoard.js
+++ src/models/LeaderBoard.js
@@ -38,10 +38,6 @@ const leaderBoardSchema = new Schema({
     type: Number,
     default: 0
   },
-  position: {
-    type: Number,
-    default: 0
-  },
   createdAt: {
     type: Date,
     default: Date.now
